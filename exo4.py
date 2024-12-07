b345585f2f560fb8863e9b6c10ca14d9d822c969
ILYES SOUALMIA <ilyessoualmia18@gmail.com>
2024-12-07 09:43:52 +0100

    exo4

---
+++ exo4.py
@@ -0,0 +1,10 @@
+age1 = int(input("Please type in the age of the first person: "))
+
+age2 = int(input("Please type in the age of the second person: "))
+
+if (age1 > age2):
+    print("The first person is older than the second person.")
+elif (age1 < age2):
+    print("The first person is younger than the second person.")
+else:
+    print("Both persons are of the same age.")
