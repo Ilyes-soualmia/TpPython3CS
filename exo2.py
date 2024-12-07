8eb4e579b4b0dfff81dd350e515854ddc3e78534
ILYES SOUALMIA <ilyessoualmia18@gmail.com>
2024-12-07 09:31:29 +0100

    exo2

---
+++ exo2.py
@@ -0,0 +1,9 @@
+Temprature = input("Enter the temprature in Celsius: ")
+
+if (int(Temprature) < 0):
+    print("It is freezing!")
+if (int(Temprature) < 10):
+    print("It is cold!")
+if (int(Temprature) < 20):    
+    print("It is cool!")
+print("Stay safe!")
