23c63d09cf67e9e0a8bb294927227c1da0232bdb
ILYES SOUALMIA <ilyessoualmia18@gmail.com>
2024-12-09 14:57:29 +0100

    modified exo5

--- exo5.py
+++ exo5.py
@@ -6,8 +6,8 @@ name2 = input("name: ")
 Time2 = float(input("Time (in seconds): "))
 
 if (Time < Time2):
-    print("The faster runner is ", name1)
+    print(f"The faster runner is {name1}")
 elif (Time > Time2):
-    print("The faster runner is ", name2)
+    print(f"The faster runner is {name2}")
 else:
-    print(name1, " and ", name2, " have the same time.")
+    print(f"{name1} and {name2} have the same time.")
