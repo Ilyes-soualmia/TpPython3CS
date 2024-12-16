dc6cfe623863d176e9d0efc97b1b455f9ce150ed
ILYES SOUALMIA <ilyessoualmia18@gmail.com>
2024-12-16 21:06:22 +0100

    exo cours 1

---
+++ exosCours/cours1.py
@@ -0,0 +1,15 @@
+hourly_wage =20.0
+hours = 6
+day = "Sunday"
+
+daily_wages = hourly_wage * hours #calculate usual daily wages
+
+# if day is Sunday, double the daily wages
+if day == "Sunday":
+    daily_wages *= 2
+
+print(daily_wages)
+
+
+
+
