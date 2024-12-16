3a1ae85b95018bf8296bed5068b2752311816a46
ILYES SOUALMIA <ilyessoualmia18@gmail.com>
2024-12-16 14:54:16 +0100

    fixed wrong inputs

--- exo16.py
+++ exo16.py
@@ -1,18 +1,20 @@
 numbers = [1, 2, 3, 4, 5]
 
 while True:
-    index = int(input("Enter index (-1 to quit): "))
+    while True:
+        index = input("Enter index (-1 to quit): ")
+        if (index.isdigit()) or (index == "-1"):
+            index = int(index)
+            break
+        else:
+            print("Invalid index")
+
     if index == -1:
         break
+    if index +1 > len(numbers) or index < 0:
+        print("Invalid input...")
     else:
-        while True:
-            if index < 0 or index >= len(numbers):
-                print("Invalid index")
-                index = int(input("Enter index (-1 to quit): "))
-            else:
-                break
-
-    value = int(input("Enter new value: "))
-    numbers[index] = value
+        value = int(input("Entre a value: "))
+        numbers[index] = value
 
 print(numbers)
