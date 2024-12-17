e0b58a2902f52bd6ea8e6a2382d8d0588f17800c
ILYES SOUALMIA <ilyessoualmia18@gmail.com>
2024-12-17 01:20:53 +0100

    exo cours 6

---
+++ exosCours/cours6.py
@@ -0,0 +1,9 @@
+while True:
+    text = input("shall we continue? ")
+    if text == "no" or text == "NO" or text == "No":
+        print("Okay then")
+        break
+    else:
+        print(text)
+
+print("okay then ")
